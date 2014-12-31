3de72692e875b1d80876e9780bc522ddc0fefd9b
M.C. Brouwer <matthijs@artandflywork.org>
2014-12-31 09:58:19 +0100

    wip

--- light_a_candle.php
+++ light_a_candle.php
@@ -8,33 +8,94 @@ Version: 1
 Author URI: http://artandflywork.com
 */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+// Prevent direct file access
+if ( ! defined ( 'ABSPATH' ) ) {
+	exit;
 }
 
-function renderCandles($cat) {
-	echo "<ul>candles from cat $cat</ul>";
+class light_a_candle extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		parent::__construct(
+	 		'light_a_candle',
+			'Light A Candle',
+			array( 'description' => 'Show candles for people in the sidebar based on titles in a specific post categorie', )
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $before_widget;
+		if ( ! empty( $title ) )
+			echo $before_title . $title . $after_title;
+		
+		// THE WIDGET STUFF
+		echo "render candles here..";
+
+		echo $after_widget;
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = 'Light A Candle';
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<p>
+			<?php wp_dropdown_categories( 'show_count=1&hierarchical=1' ); ?>
+		</p>
+		<?php 
+	}
+
 }
 
-function widget_candles($args) {
- extract($args);
- echo $before_widget;
- echo $before_title;
- echo $after_title;
- renderCandles(9); // render alle candles uit category 9
- echo $after_widget;
-}
+// run the widget
+add_action( 'widgets_init', create_function( '', 'register_widget("light_a_candle");' ) );
 
-function candlesInit() {
-  wp_register_sidebar_widget(
-    'light_a_candle',
-    'Light A Candle',
-    'widget_candles',
-    array(
-        'description' => 'Show candles for people in the sidebar based on titles in a specific post categorie'
-    )
-	);
-}
 
-add_action("plugins_loaded", "candlesInit");
+
+
+
